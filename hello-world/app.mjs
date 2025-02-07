a4e123a78c13b390c540f378f065e574d508d3b4
stuaiki <takaku@byu.edu>
2025-02-07 04:43:08 -0700

    Breaking the lambda function on purpose

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,23 +1,18 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+let response;
 
-export const lambdaHandler = async (event, context) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "I'm using canary deployments",
-    }),
-  };
+exports.lambdaHandler = async (event, context) => {
+  throw new Error("This will cause a deployment rollback");
+  // try {
+  //     response = {
+  //         "statusCode": 200,
+  //         "body": JSON.stringify({
+  //             message: "I'm using canary deployments",
+  //         })
+  //     }
+  // } catch (err) {
+  //     console.log(err);
+  //     return err;
+  // }
 
-  return response;
+  // return response
 };
